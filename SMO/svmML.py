54ca6bd9308e978031c859ae9e9f2d1cd67318bf
DESKTOP-BQL46S4\sonny <zhangzheming_282@163.com>
2016-12-13 00:42:45 +0800

    add SMO

---
+++ SMO/svmML.py
@@ -0,0 +1,22 @@
+# encoding:utf-8
+import random
+
+
+def loadData(filename):
+    dataMat = []
+    labelMat = []
+    fr = open(filename)
+    for line in fr.readlines():
+        lineArr = line.strip().split("\t")
+        dataMat.append([float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(float(lineArr[2]))
+    return dataMat,labelMat
+
+def selectJrand(i,m):
+    j = i
+    while(j == i):
+        j = int(random.uniform(0,m))
+    return j
+
+if __name__ == "__main__":
+    dataMat, labelMat = loadData("testSet.txt")
