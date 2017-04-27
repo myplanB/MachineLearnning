6f4fea3d4f75911191fdce946e2774e4491f3906
SONNY\zheming <zhangzheming_282@163.com>
2017-04-27 23:12:54 +0800

    add pca  function

---
+++ PANDAS/__init__.py
@@ -0,0 +1,10 @@
+"""
+@version: latest
+@author: sonny
+@license: Apache Licence 
+@contact: 715676363@qq.com
+@site: 
+@software: PyCharm Community Edition
+@file: __init__.py.py
+@time: 2017/4/27 21:26
+"""
---
+++ PANDAS/test01.py
@@ -0,0 +1,33 @@
+"""
+@version: latest
+@author: sonny
+@license: Apache Licence 
+@contact: 715676363@qq.com
+@site: 
+@software: PyCharm Community Edition
+@file: test01.py
+@time: 2017/4/27 21:42
+"""
+import pandas as pd
+
+unames = ['user_id','gender','age','occupation','zip']
+
+users = pd.read_table('users.dat',sep='::',header=None,names=unames)
+
+
+rnames = ['user_id','movie_id','rating','timestamp']
+
+ratings = pd.read_table('ratings.dat',sep='::',header=None,names=rnames)
+
+
+
+mnames = ['movie_id','title','genres']
+
+movies = pd.read_table('movies.dat',sep='::',header=None,names=mnames)
+
+
+data = pd.merge(pd.merge(ratings,users),movies)
+
+print data.pivot_table('rating',rows='title',cols='gender',aggfunc='mean')
+
+# print data.groupby('title').size()
